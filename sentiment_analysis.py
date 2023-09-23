d0ffb71681597b58c26a666ab154b00e60b6d767
Žygimantas Krasauskas <zygimantaskrasauskas@Zygimantass-MacBook-Pro.local>
2023-09-23 10:38:56 +0200

    another sentiment model and comparison between them

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,14 +1,21 @@
 from textblob import TextBlob
 import pandas as pd
 
-# Read data into DataFrame
-df_all_africa: pd.DataFrame = pd.read_csv("data/all_africa_southsudan.csv")
+import spacy
+from spacytextblob.spacytextblob import SpacyTextBlob
+
+nlp = spacy.load("en_core_web_sm")
 
 # Apply sentiment analysis to each article within a column and set it to either 1 0 or -1
-def sentiment_analysis(df: pd.DataFrame, column: str) -> pd.DataFrame:
+def textblob_sentiment_analysis(df: pd.DataFrame, column: str) -> pd.DataFrame:
     df["sentiment"] = df[column].apply(lambda x: TextBlob(x).sentiment.polarity)
     df["sentiment"] = df["sentiment"].apply(lambda x: 1 if x > 0 else 0 if x == 0 else -1)
-    df.to_csv("data/all_africa_southsudan.csv", index=False)
+    return df
 
-# Apply sentiment analysis to the "paragraphs" column
-sentiment_analysis(df_all_africa, "paragraphs")
+# Apply spacytextblob sentiment analysis on articles within a column and set the outcomes to either 1 0 or -1
+def spacy_sentiment_analysis(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    nlp.add_pipe("spacytextblob")
+    df["sentiment"] = df[column].apply(lambda x: nlp(x)._.polarity)
+    df["subjectivity"] = df[column].apply(lambda x: nlp(x)._.subjectivity)
+    df["sentiment"] = df["sentiment"].apply(lambda x: 1 if x > 0 else 0 if x == 0 else -1)
+    return df
