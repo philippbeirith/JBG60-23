ab6a6497fed49d6ba2d8e8a828f7b9820ffe4b48
Žygimantas Krasauskas <zygimantaskrasauskas@Zygimantass-MacBook-Pro.local>
2023-09-23 20:38:48 +0200

    changes to the code

--- forecasting.py
+++ forecasting.py
@@ -11,32 +11,45 @@ from sklearn.ensemble import RandomForestClassifier
 from scripts import preprocessing
 from scripts import evaluation
 
-
 ### Begin with pre processing ###
 # Read data into DataFrame
-df_food_crises_cleaned: pd.DataFrame = pd.read_csv("data/food_crises_cleaned.csv")
-df_southsudan: pd.DataFrame = pd.read_csv("data/all_africa_southsudan.csv")
-
-#Modify datasets
-df_food_crises_cleaned = preprocessing.backfill_ipc(df_food_crises_cleaned)
-df_southsudan = preprocessing.sentiment_analysis(df_southsudan, 'paragraphs')
+df_crises = pd.read_csv("data/food_crises_cleaned.csv")
+df_summary = pd.read_csv("data/articles_summary_cleaned.csv")
 
 #Augment
-df_food_crises_cleaned = preprocessing.calculate_crises_metrics(df_food_crises_cleaned)
-df_southsudan = preprocessing.calculate_news_metrics(df_southsudan).reset_index()
+df_crises = preprocessing.calculate_crises_metrics(df_crises)
+
+#Modify datasets
+df_crises = preprocessing.backfill_ipc(df_crises)
+df_crises['date'] = pd.to_datetime(df_crises['date']).dt.strftime('%Y-%m')
+df_crises['date'] = df_crises['date'].astype(str)
+df_crises.drop(columns=["country", "district_code", "centx", "centy", "year_month", "year", "month", "ha"], inplace=True)
+
+df_summary = preprocessing.sentiment_analysis(df_summary, "summary")
+df_summary.drop(columns=["summary", "lat", "lng"], inplace=True)
+df_summary.rename(columns={"location_article": "district"}, inplace=True)
+df_summary['date'] = pd.to_datetime(df_summary['date']).dt.strftime('%Y-%m')
+df_summary['date'] = df_summary['date'].astype(str)
 
 #Consolidate
-dataset: pd.DataFrame = preprocessing.consolidate_data(df_southsudan, df_food_crises_cleaned)
-dataset=dataset[dataset['ipc_delta']!=0]
+model_dataset = preprocessing.consolidate_data(df_summary, df_crises)
+model_dataset = model_dataset.dropna(subset=['ipc'])
+model_dataset['date'] = pd.to_datetime(model_dataset['date'])
+model_dataset.drop(columns=["date"], inplace=True)
+
+district_names = model_dataset["district"].unique()
+district_mapping = {name: index for index, name in enumerate(district_names)}
+int_data = [district_mapping[name] for name in model_dataset["district"]]
+model_dataset["district"] = int_data
+
+model_dataset = model_dataset.dropna()
 
 ### Feature Selection ###
 #test for longevity
 #dataset_val = dataset[dataset['date'] >= '2023-01-01']
 
-dataset.drop(columns=['date', 'district', 'country', 'year_month', 'ha'], inplace = True)
-
-model_lead_1 = evaluation.test_random_forest_classification_performance(dataset.drop(columns=['ipc_lead_1', 'ipc_lead_3', 'ipc_lead_6']), dataset['ipc_lead_1'], test_size=0.2, random_state=23)
+model_lead_1 = evaluation.test_random_forest_classification_performance(model_dataset.drop(columns=['ipc_lead_1', 'ipc_lead_3', 'ipc_lead_6']), model_dataset['ipc_lead_1'], test_size=0.2, random_state=23)
 print('############################################################')
-model_lead_3 = evaluation.test_random_forest_classification_performance(dataset.drop(columns=['ipc_lead_1', 'ipc_lead_3', 'ipc_lead_6']), dataset['ipc_lead_3'], test_size=0.2, random_state=23)
-print('############################################################')
-model_lead_6 = evaluation.test_random_forest_classification_performance(dataset.drop(columns=['ipc_lead_1', 'ipc_lead_3', 'ipc_lead_6']), dataset['ipc_lead_6'], test_size=0.2, random_state=23)
+#model_lead_3 = evaluation.test_random_forest_classification_performance(dataset.drop(columns=['ipc_lead_1', 'ipc_lead_3', 'ipc_lead_6']), dataset['ipc_lead_3'], test_size=0.2, random_state=23)
+#print('############################################################')
+#model_lead_6 = evaluation.test_random_forest_classification_performance(dataset.drop(columns=['ipc_lead_1', 'ipc_lead_3', 'ipc_lead_6']), dataset['ipc_lead_6'], test_size=0.2, random_state=23)
--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -116,67 +116,12 @@ def sentiment_analysis(df: pd.DataFrame, column: str) -> pd.DataFrame:
     df["sentiment"] = df[column].apply(lambda x: TextBlob(x).sentiment.polarity)
     df["sentiment"] = df["sentiment"].apply(lambda x: 1 if x > 0 else 0 if x == 0 else -1)
     return(df)
-
-# This helper function was taken from the 'predicting' jupyter notebook from https://github.com/GielJW/JBG060-DC3-23-24-public/tree/main
-def create_lag_df(df, columns, lag, difference=False, rolling=None, dropna=False):
-    '''
-    Function to add lagged colums to dataframe
-    
-    Inputs:
-        df - Dataframe
-        columns - List of columns to create lags from
-        lag - The number of timesteps (in months for the default data) to lag the variable by
-        difference - Whether to take the difference between each observation as new column
-        rolling - The size of the rolling mean window, input None type to not use a rolling variable
-        dropna - Whether to drop NaN values
-        
-    Output:
-        df - Dataframe with the lagged columns added
-    '''
-    
-    for column in columns:
-        col = df[column].unstack()
-        if rolling:
-            col = col.rolling(rolling).mean()
-        if difference:
-            col = col.diff()
-        if dropna:
-            col = col.dropna(how='any')
-        df[f"{column}_lag_{lag}"] = col.shift(lag).stack()
-    return df
-
-#This functions aims to quantify and parameterize the news article dataset such that it can be used in random forest.
-def calculate_news_metrics(df: pd.DataFrame):
-    #The final version of this dataset needs to be an aggregate per month
-    
-    #count(*) of all articles in a month
-    article_count = df.groupby('date').count()
-    
-    #count(distinct *) of unique publishers
-    publisher_count = df.groupby('date')['publisher'].nunique()
-    output_df = pd.merge(article_count,publisher_count,on='date')
-    
-    #percent_negative bounds -1:-.33
-    #percent_negative = publisher_count
-    #output_df = pd.merge(output_df,percent_negative,on='date')
-    
-    #percent_neutral bounds -.33:.33
-    #percent_neutral = publisher_count
-    #output_df = pd.merge(output_df,percent_neutral,on='date')
-    
-    #percent_positive bound .34:1
-    #percent_positive = publisher_count
-    #output_df = pd.merge(output_df,percent_positive,on='date')
-    
-    #nlp to be added later
-    
-    return(output_df)
     
 def calculate_crises_metrics(df: pd.DataFrame):
+
     #Add crisis level delta (if changed, then by how many levels?)
     df = calculate_delta(df, col = "ipc")
 
-    
     #Add 'months_since_change' 
     df = calculate_months_since_change(df, col = "ipc")
 
@@ -191,7 +136,6 @@ def calculate_crises_metrics(df: pd.DataFrame):
     df = apply_lag(df, col_list = cols, lag = 3)
     df = apply_lag(df, col_list = cols, lag = 6)
     
-    
     df = apply_lead(df, col_list = cols, lead = 1)
     df = apply_lead(df, col_list = cols, lead = 3)
     df = apply_lead(df, col_list = cols, lead = 6)
@@ -202,15 +146,11 @@ def calculate_crises_metrics(df: pd.DataFrame):
     df = calculate_rain_bool(df)
     df = calculate_ndvi_bool(df)
     df = calculate_et_bool(df)
+
     return(df)
 
 #This consolidates the food_crises metrics and all_africa_southsudan dataset, ready to be fed into the model.
-def consolidate_data(southsudan: pd.DataFrame, crisis: pd.DataFrame):
-    southsudan['date'] = southsudan['date'].astype('str')
-    crisis['date'] = crisis['date'].astype('str')
-    output_df = southsudan.merge(crisis, on='date')
-    output_df['country_code'] = np.where(output_df['country'].astype('str') == 'South Sudan', 1, 2)
+def consolidate_data(summary: pd.DataFrame, crisis: pd.DataFrame):
 
-        
-    
+    output_df = pd.merge(summary, crisis, how='left', on=['date','district'])
     return(output_df)
